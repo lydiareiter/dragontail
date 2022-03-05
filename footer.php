7a58269a273308f6c8389bde24d0956227072f91
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-03-05 22:18:08 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 <footer>
+    <h2>Lydia Reiter</h2>
     <div>
-        <h2>Lydia Reiter</h2>
-    </div>
-    <div>
-        <h2>Über mich</h2>
-    </div>
-    <div>
-        <h2>Kontakt</h2>
-        <a href="mailto:lydia.maslowski@gmail.com">lydia.maslowski@gmail.com</a>
+        <div>
+            <h2>Über mich</h2>
+            <p>Schule: <a href="https://www.htl-leonding.at/">HTBLA Leonding</a></p>
+        </div>
+        <div>
+            <h2>Kontakt</h2>
+            <a href="mailto:lydia.maslowski@gmail.com">lydia.maslowski@gmail.com</a>
+        </div>
     </div>
 </footer>
