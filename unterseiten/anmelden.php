1d31db3d46036fffc1bbf08d391d4817a7a4a09d
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-03-05 15:49:03 +0100

    artikel added

---
+++ unterseiten/anmelden.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Open+Sans:wght@300&family=Work+Sans:wght@200&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
+    <title>Dragontail</title>
+</head>
+
+<body>
+    <div id="header">
+        <div id="grun"></div>
+        <a href="../index.php">
+            <h3>Dragontail</h3>
+        </a>
+        <h1>Anmeldung</h1>
+        <div id="buttons">
+            <a href="./warenkorb.php">
+                <img src="../img/shopping-cart.png" alt="Shopping Cart">
+            </a>
+            <a href="../index.php">
+                <img src="../img/x.png" alt="close">
+            </a>
+        </div>
+    </div>
+    <div id="mainBox">
+        <div>
+            <h3>E-Mail</h3>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <h3>Passwort</h3>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <button type="submit">Anmelden</button>
+        </div>
+        <div>
+            <a href="./konto-erstellen.php">Konto erstellen</a>
+        </div>
+
+    </div>
+</body>
+
+</html>
