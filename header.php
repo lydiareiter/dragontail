de61347dc23cce61ed114eaee2bd001b6498f026
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-03-23 12:24:17 +0100

    added header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Open+Sans:wght@300&family=Work+Sans:wght@200&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="icon" type="image/vnd.microsoft.icon" href="./img/favicon/favicon-96x96.ico">
+    <link rel="icon" type="image/png" href="./img/favicon/mstile-310x310.png">
+
+    <title>Dragontail</title>
+</head>
