1d2cdb185441ccf206f47e49bc9373834e950305
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-03-24 09:28:35 +0100

    api vorbereitet

--- index.php
+++ index.php
@@ -21,21 +21,7 @@ include("./headerIndex.php");
 
 <div id="artikel">
     <?php
-
-    $artikel = json_decode(file_get_contents("./artikel.json"));
-
-    foreach ($artikel as $i) {
-        $pfad = "./img/artikel/" . $i->katigorie . "/";
-        $string = '<div onclick="popUp(this)" class="artikeln" id="' . $i->artikelnr . '">';
-        $string2 = "<div style=\"background-image: url('$pfad$i->img')\"></div>";
-        $string3 = '<p>' . $i->titel . '</p>';
-        $string4 = '</div>';
-
-        echo $string;
-        echo $string2;
-        echo $string3;
-        echo $string4;
-    }
+    include("./api.php")
     ?>
 </div>
 
