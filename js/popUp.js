d875abff1579d8a59262d5d6a622e4a5647809c1
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-03-24 08:59:54 +0100

    pop up added with css

--- js/popUp.js
+++ js/popUp.js
@@ -1,16 +1,14 @@
 let isPopedUp = false;
 let blurry = document.getElementById('blurry');
+let opend = document.getElementById('opend');
+let idOpend;
 
 function popUp(elem) {
     if (!isPopedUp) {
         isPopedUp = true;
 
-        let heightBox = elem.offsetHeight;
-        let widthBox = elem.offsetWidth;
-
-        elem.style.position = "absolute";
-        elem.style.height = heightBox + "px";
-        elem.style.width = widthBox + "px";
+        idOpend = elem.id;
+        elem.id = "opend";
 
         let tempHeight = window.innerHeight;
         let tempWidth = window.innerWidth;
@@ -21,12 +19,9 @@ function popUp(elem) {
         let tempTop = window.innerHeight/2 - tempHeight/2 - 0;
         let tempLeft = window.innerWidth/2 - tempWidth/2 - 0;
 
-        elem.style.height = tempHeight + "px";
-        elem.style.width = tempWidth + "px";
-
         elem.style.top = tempTop + "px";
         elem.style.left = tempLeft + "px";
 
-        console.log(elem);
+        elem
     }
 }
