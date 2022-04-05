8a06dea97c5b60bc44d5266b0370cb8459829c49
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-04-05 18:56:19 +0200

    login per db

--- unterseiten/login.php
+++ unterseiten/login.php
@@ -1,19 +1,38 @@
 <?php
-$benutzer = json_decode(file_get_contents("../benutzer.json"));
 
-foreach ($benutzer as $i) {
-    if ($i->email == $_POST["email"] && $i->passwort == md5($_POST["password"])) {
-        $string2 = "{";
-        $string2 = $string2 . '"vorname": "' . $i->vorname . '",';
-        $string2 = $string2 . '"nachname": "' . $i->nachname . '",';
-        $string2 = $string2 . '"geburtsdatum": "' . $i->geburtsdatum . '",';
-        $string2 = $string2 . '"email": "' . $i->email . '",';
-        $string2 = $string2 . '"passwort": "' . $i->passwort . '"';
-        $string2 = $string2 . "}";
+$_db_host = "localhost";
+$_db_datenbank = "dragontail";
+$_db_username = "dev";
+$_db_passwort = "dev1234";
+$conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
 
-        setcookie("Dragontail", $string2, time() + 3600); # 1 Stunde gültig
-        header("Location: ./profil.php");
-        exit;
-    }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-?>
+
+// Get Data
+$sql = "SELECT * FROM benutzer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row["vorname"] . " " . $row["nachname"] . " " . $row["geburtsdatum"] . " " . $row["email"] . " " . $row["passwort"] . "<br>";
+
+        if ($row["email"] == $_POST["email"] && $row["passwort"] == md5($_POST["password"])) {
+            $string2 = "{";
+            $string2 = $string2 . '"vorname": "' . $row["vorname"] . '",';
+            $string2 = $string2 . '"nachname": "' . $row["nachname"] . '",';
+            $string2 = $string2 . '"geburtsdatum": "' . $row["geburtsdatum"] . '",';
+            $string2 = $string2 . '"email": "' . $row["email"] . '",';
+            $string2 = $string2 . '"passwort": "' . $row["passwort"] . '"';
+            $string2 = $string2 . "}";
+
+            setcookie("Dragontail", $string2, time() + 3600); # 1 Stunde gültig
+            header("Location: ./profil.php");
+            exit;
+        }
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
