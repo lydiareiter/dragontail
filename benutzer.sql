30a028453b7e73fc829a703e8a85da4135de0a35
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-04-05 18:01:23 +0200

    insert added

---
+++ benutzer.sql
@@ -0,0 +1,3 @@
+INSERT INTO benutzer(vorname,nachname,geburtsdatum,email,passwort) VALUES ('Lydia','Reiter','2004-03-13','lydia.maslowski@gmail.com','16d7a4fca7442dda3ad93c9a726597e4');
+INSERT INTO benutzer(vorname,nachname,geburtsdatum,email,passwort) VALUES ('Thomas','Spindler','2005-03-17','t.spindler@students.htl-leonding.ac.at','16d7a4fca7442dda3ad93c9a726597e4');
+INSERT INTO benutzer(vorname,nachname,geburtsdatum,email,passwort) VALUES ('Lydia','Maslowski','2004-03-13','l.maslowski@gmx.at','16d7a4fca7442dda3ad93c9a726597e4');
