f26f2b0e3fcacad61be7dde543d123deec8d6b1a
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2022-04-06 09:35:54 +0200

    Update benutzerErstellen.php

--- unterseiten/benutzerErstellen.php
+++ unterseiten/benutzerErstellen.php
@@ -1,32 +1,40 @@
 <?php
-$benutzer = json_decode(file_get_contents("../benutzer.json"));
+$_db_host = "localhost";
+$_db_datenbank = "dragontail";
+$_db_username = "dev";
+$_db_passwort = "dev1234";
+$conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
 
-$string = "[";
-foreach ($benutzer as $i) {
-    $string = $string . '{';
-    $string = $string . '"vorname": "' . $i->vorname . '",';
-    $string = $string . '"nachname": "' . $i->nachname . '",';
-    $string = $string . '"geburtsdatum": "' . $i->geburtsdatum . '",';
-    $string = $string . '"email": "' . $i->email . '",';
-    $string = $string . '"passwort": "' . $i->passwort . '"';
-    $string = $string . '},';
+if ($conn->connect_error) {
+    echo "Fehler";
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$string2 = "{";
-$string2 = $string2 . '"vorname": "' . $_POST["vorname"] . '",';
-$string2 = $string2 . '"nachname": "' . $_POST["nachname"] . '",';
-$string2 = $string2 . '"geburtsdatum": "' . $_POST["gbDate"] . '",';
-$string2 = $string2 . '"email": "' . $_POST["email"] . '",';
-$string2 = $string2 . '"passwort": "' . md5($_POST["password"]) . '"';
-$string2 = $string2 . "}";
+// Get Data
+$sql = "insert into benutzer (vorname, nachname, geburtsdatum, email, passwort) values (" . 
+    $_POST["vorname"] .
+    ", " .
+    $_POST["nachname"] .
+    ", " .
+    $_POST["gbDate"] .
+    ", " .
+    $_POST["email"] .
+    ", " .
+    md5($_POST["password"]) .
+    ")";
+    echo 2;
+$result = $conn->query($sql);
+$conn->query("commit");
+$conn->close();
 
-$string = $string . $string2 . "]";
+$string = "{";
+    $string = $string . '"vorname": "' . $_POST["vorname"] . '",';
+    $string = $string . '"nachname": "' . $_POST["nachname"] . '",';
+    $string = $string . '"geburtsdatum": "' . $_POST["gbDate"] . '",';
+    $string = $string . '"email": "' . $_POST["email"] . '",';
+    $string = $string . '"passwort": "' . md5($_POST["password"]) . '"';
+    $string = $string . "}";
 
-$myfile = fopen("../benutzer.json", "w") or die("Unable to open file!");
-fwrite($myfile, $string);
-fclose($myfile);
-
-setcookie("Dragontail", $string2, time() + 3600); # 1 Stunde gültig
+setcookie("Dragontail", $string, time() + 3600); # 1 Stunde gültig
 header("Location: ./profil.php");
-exit;
 ?>
